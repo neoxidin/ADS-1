f67e3fa3d0c812565d6a04a2440efdbea2485279
neoxidin <vanya.robotov@gmail.com>
2026-02-27 15:01:41 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,79 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <cmath>
+#include <iostream>
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+	if (value==0){
+		std::cout << "Enter number: ";
+		std::cin >> value;
+	}
+	for (int i = 2; i <= std::sqrt(value); ++i) {
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+	std::cout << "Enter number: ";
+	std::cin >> n;
+	int count = 0;
+	int i = 2;
+	while (true) {
+		if (checkPrime(i)) {
+			count++;
+			if (count == n) {
+				return i;
+			}
+		}
+		i++;
+	}
+	return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+	std::cout << "Enter number: ";
+	std::cin >> value;
+	if (checkPrime(value)) {
+		value++;
+	}
+	while (true) {
+		if (checkPrime(value)) {
+			return value;
+		}
+		value++;
+	}
+	return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	std::cout << "Enter number: ";
+	std::cin >> hbound;
+	int i = 2;
+	int sum = 0;
+	while (i<hbound) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
+		if (i+1 == hbound) {
+			return sum;
+		}
+		i++;
+	}
+	return 2;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+	std::cout << "Enter range: ";
+	std::cin >> lbound >> hbound;
+	int count = 0;
+	for (int i = lbound; i+2 < hbound; i++) {
+		if (checkPrime(i) && checkPrime(i+2)) {
+			count++;
+		}
+	}
+	return count;
 }
